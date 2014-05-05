224cf3820ad678632364b84eeb7569ca4b6c6105
goliatone <burgosemi@gmail.com>
2014-05-05 11:11:44 -0400

    example, updated spacing.

--- examples/main.js
+++ examples/main.js
@@ -7,110 +7,123 @@ requirejs.config({
     }
 });
 
-define(['go2o', 'jquery'], function (Go2o, $) {
+define(['go2o', 'jquery'], function(Go2o, $) {
     console.log('Loading');
 
-	/*
+    /*
      * WE HAVE OUR SOURCE OBJECT.
-    */
+     */
     var json = {
         "type": "event",
         "name": "config_available",
         "scene": "3b-provisions",
-        "payload":
-          {
+        "payload": {
             "bill": {
-                "_id" : "535a7950e4b07d4c5ee63a54",
+                "_id": "535a7950e4b07d4c5ee63a54",
                 "title": "This is a title",
                 "billPurpose": "this is nothing",
-                "provisions" : {
-                    "provisions" : [
-                      {
-                        "_id" : "534715f2e4b052a43d84e922",
-                        "name" : "Heavily Tax Fracking Companies",
-                        "impacts" : [
-                            {
-                              "_id" : "532b0ceee4b0d782629bc623",
-                              "name" : "Jobs",
-                              "direction" : "DOWN"
-                            },
-                            {
-                              "_id" : "532afb06e4b0d782629bc5c5",
-                              "name" : "Environment",
-                              "direction" : "NEUTRAL"
-                            },
-                            {
-                              "_id" : "532afab7e4b0d782629bc5bf",
-                              "name" : "Oil",
-                              "direction" : "DOWN"
-                            }
-                        ]
-                      }
-                   ]
+                "provisions": {
+                    "provisions": [{
+                        "_id": "534715f2e4b052a43d84e922",
+                        "name": "Heavily Tax Fracking Companies",
+                        "impacts": [{
+                            "_id": "532b0ceee4b0d782629bc623",
+                            "name": "Jobs",
+                            "direction": "DOWN"
+                        }, {
+                            "_id": "532afb06e4b0d782629bc5c5",
+                            "name": "Environment",
+                            "direction": "NEUTRAL"
+                        }, {
+                            "_id": "532afab7e4b0d782629bc5bf",
+                            "name": "Oil",
+                            "direction": "DOWN"
+                        }]
+                    }]
                 }
             },
-            "committee":
-            {
-              "_id" : "5339c243e4b01dbc9cf97c00",
-              "name" : "Bill Building Committee",
-              "committeeType" : "BILL_BUILDING",
-              "subCommittees" : [
-                      {
-                        "_id" : "5339c24be4b01dbc9cf97c02",
-                        "name" : "Great Subcommittee",
-                        "provisions" : [
-                            {
-                              "_id" : "533a483de4b0c5a5e15150f4",
-                              "name" : "Provision Whatever",
-                              "impacts" : [ 'impact1','impact2' ]
-                            }
-                        ],
-                        "hearingQuestions" : [
-                            {id:1, question:"hello?"},
-                            {id:2, question:"what?"},
-                            {id:3, question:"who?"},
-                        ],
-                        "expertInfo" : {
-                        "nomineeName" : "Lorem ipsum here, nothing more",
-                        "nomineeTitle" : "Lorem ipsum here, nothing more",
-                        "nomineeFor" : "Lorem ipsum here, nothing more",
-                        "nomineeBio" : "Lorem ipsum here, nothing more"
-                      }
+            "committee": {
+                "_id": "5339c243e4b01dbc9cf97c00",
+                "name": "Bill Building Committee",
+                "committeeType": "BILL_BUILDING",
+                "subCommittees": [{
+                    "_id": "5339c24be4b01dbc9cf97c02",
+                    "name": "Great Subcommittee",
+                    "provisions": [{
+                        "_id": "533a483de4b0c5a5e15150f4",
+                        "name": "Provision Whatever",
+                        "impacts": ['impact1', 'impact2']
+                    }],
+                    "hearingQuestions": [{
+                        id: 1,
+                        question: "hello?"
+                    }, {
+                        id: 2,
+                        question: "what?"
+                    }, {
+                        id: 3,
+                        question: "who?"
+                    }, ],
+                    "expertInfo": {
+                        "nomineeName": "Lorem ipsum here, nothing more",
+                        "nomineeTitle": "Lorem ipsum here, nothing more",
+                        "nomineeFor": "Lorem ipsum here, nothing more",
+                        "nomineeBio": "Lorem ipsum here, nothing more"
                     }
-                ],
-                "amendments" : [
-                      {
-                        "_id" : "53458344e4b07375b113fab0",
-                        "name" : "Gun Control",
-                        "party" : {
-                          "_id" : "534582f7e4b07375b113faa2",
-                          "name" : "Republican",
-                          "impacts" : []
-                        },
-                        "faction" : {
-                          "_id" : "5339c22be4b01dbc9cf97bfe",
-                          "name" : "Republic Faction"
-                        },
-                        "negativeExplanation" : "Suck it",
-                        "impacts" : [
-                            {
-                              "_id" : "5339c212e4b01dbc9cf97bf6",
-                              "name" : "Impact 2 Updated",
-                              "direction" : "UP"
-                            },
-                            {
-                              "_id" : "5339c218e4b01dbc9cf97bf8",
-                              "name" : "Impact 3",
-                              "direction" : "UP"
-                            },
-                            {
-                              "_id" : "5339c21de4b01dbc9cf97bfa",
-                              "name" : "Impact 4 Updated",
-                              "direction" : "UP"
-                            }
-                        ]
-                    }
-                ]
+                }],
+                "amendments": [{
+                    "_id": "53458344e4b07375b113fab0",
+                    "name": "Gun Control",
+                    "party": {
+                        "_id": "534582f7e4b07375b113faa2",
+                        "name": "Republican",
+                        "impacts": []
+                    },
+                    "faction": {
+                        "_id": "5339c22be4b01dbc9cf97bfe",
+                        "name": "Republic Faction"
+                    },
+                    "negativeExplanation": "Suck it",
+                    "impacts": [{
+                        "_id": "5339c212e4b01dbc9cf97bf6",
+                        "name": "Impact 2 Updated",
+                        "direction": "UP"
+                    }, {
+                        "_id": "5339c218e4b01dbc9cf97bf8",
+                        "name": "Impact 3",
+                        "direction": "UP"
+                    }, {
+                        "_id": "5339c21de4b01dbc9cf97bfa",
+                        "name": "Impact 4 Updated",
+                        "direction": "UP"
+                    }]
+                }, {
+                    "_id": "53453333e4b07375b113fab3",
+                    "name": "Pet Control",
+                    "party": {
+                        "_id": "534582f7e4b07375b113fbbb",
+                        "name": "Republican",
+                        "impacts": []
+                    },
+                    "faction": {
+                        "_id": "5339c22be4b01dbc9cf97fff",
+                        "name": "Republic Faction"
+                    },
+                    "negativeExplanation": "Fuck it",
+                    "impacts": [{
+                        "_id": "5339c212e4b01dbc9cf97bf3",
+                        "name": "Impact 5 Updated",
+                        "direction": "UP"
+                    }, {
+                        "_id": "5339c218e4b01dbc9cf97bf1",
+                        "name": "Impact 6",
+                        "direction": "UP"
+                    }, {
+                        "_id": "5339c21de4b01dbc9cf97bfb",
+                        "name": "Impact 7 Updated",
+                        "direction": "UP"
+                    }]
+                }]
             }
         }
     };
@@ -123,27 +136,27 @@ define(['go2o', 'jquery'], function (Go2o, $) {
      * Here we describe the schema that will
      * map our transformations.
      */
-    function makeSchemaTransform(data){
+    function makeSchemaTransform(data) {
         /*
          * Schema holds the transformations
          * we want to apply.
          */
         var schema = {
-            pre:  ['flattenPaths', 'renameAllIds', 'unwrapProvisions'],
+            pre: ['flattenPaths', 'renameAllIds', 'unwrapProvisions'],
             post: ['mergeOrphans', 'unflattenPaths'],
-            preprocessors:{
-                renameAllIds:function(){
-                    Object.keys(this.flattened).forEach(function(path){
-                        if(!path.match(/_id/)) return;
+            preprocessors: {
+                renameAllIds: function() {
+                    Object.keys(this.flattened).forEach(function(path) {
+                        if (!path.match(/_id/)) return;
                         var out = path.replace(/_id/, 'id'),
                             value = this.flattened[path];
                         this.flattened[out] = value;
                         delete this.flattened[path];
                     }, this);
                 },
-                unwrapProvisions: function(){
-                    Object.keys(this.flattened).forEach(function(path){
-                        if(!path.match(/payload.bill.provisions.provisions/)) return;
+                unwrapProvisions: function() {
+                    Object.keys(this.flattened).forEach(function(path) {
+                        if (!path.match(/payload.bill.provisions.provisions/)) return;
                         var out = path.replace(/payload.bill.provisions.provisions/, 'payload.bill.provisions'),
                             value = this.flattened[path];
                         this.flattened[out] = value;
@@ -151,24 +164,30 @@ define(['go2o', 'jquery'], function (Go2o, $) {
                     }, this);
                 }
             },
-            schema:{
-                'payload.bill.billPurpose':{
-                    rename:'payload.bill.description'
+            transforms: {
+                'payload.bill.billPurpose': {
+                    rename: 'payload.bill.description'
                 },
-                'payload.bill.title':{
-                    rename:'payload.bill.name'
+                'payload.bill.title': {
+                    rename: 'payload.bill.name'
                 },
-                address1:{
-                    collapse:'direccion'
+                address1: {
+                    collapse: 'direccion'
                 },
-                address2:{
-                    collapse:'direccion'
+                address2: {
+                    collapse: 'direccion'
                 },
-                'payload.committee.committeeType':{
-                    remove:true
+                'payload.committee.committeeType': {
+                    remove: false
                 },
-                'payload.committee.subCommittees':{
-                    remove: true
+                'payload.committee.subCommittees': {
+                    remove: false
+                },
+                'payload\.committee\.amendments\.\d+\.party': {
+                    remove: {
+                        glob: true,
+                        pattern: /payload\.committee\.amendments\.\d+\.party/
+                    }
                 }
             }
         };
