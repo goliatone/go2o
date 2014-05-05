4aa8675f1c11cd25278c2aeec0c20ef422f71a99
goliatone <burgosemi@gmail.com>
2014-05-05 09:21:46 -0400

    Update example with a more involved structure

--- examples/main.js
+++ examples/main.js
@@ -14,23 +14,103 @@ define(['go2o', 'jquery'], function (Go2o, $) {
      * WE HAVE OUR SOURCE OBJECT.
     */
     var json = {
-        _id:22222,
-        user:{
-            _id:33333,
-            name:'someting'
-        },
-        name:'peperone',
-        age:33,
-        city:'New York',
-        address1:'dekalb avenue',
-        address2:'apt 34b',
-        something:{
-            deep:{
-                to:{
-                    see:{
-                        merging:true
-                    }
+        "type": "event",
+        "name": "config_available",
+        "scene": "3b-provisions",
+        "payload":
+          {
+            "bill": {
+                "_id" : "535a7950e4b07d4c5ee63a54",
+                "title": "This is a title",
+                "billPurpose": "this is nothing",
+                "provisions" : {
+                    "provisions" : [
+                      {
+                        "_id" : "534715f2e4b052a43d84e922",
+                        "name" : "Heavily Tax Fracking Companies",
+                        "impacts" : [
+                            {
+                              "_id" : "532b0ceee4b0d782629bc623",
+                              "name" : "Jobs",
+                              "direction" : "DOWN"
+                            },
+                            {
+                              "_id" : "532afb06e4b0d782629bc5c5",
+                              "name" : "Environment",
+                              "direction" : "NEUTRAL"
+                            },
+                            {
+                              "_id" : "532afab7e4b0d782629bc5bf",
+                              "name" : "Oil",
+                              "direction" : "DOWN"
+                            }
+                        ]
+                      }
+                   ]
                 }
+            },
+            "committee":
+            {
+              "_id" : "5339c243e4b01dbc9cf97c00",
+              "name" : "Bill Building Committee",
+              "committeeType" : "BILL_BUILDING",
+              "subCommittees" : [
+                      {
+                        "_id" : "5339c24be4b01dbc9cf97c02",
+                        "name" : "Great Subcommittee",
+                        "provisions" : [
+                            {
+                              "_id" : "533a483de4b0c5a5e15150f4",
+                              "name" : "Provision Whatever",
+                              "impacts" : [ 'impact1','impact2' ]
+                            }
+                        ],
+                        "hearingQuestions" : [
+                            {id:1, question:"hello?"},
+                            {id:2, question:"what?"},
+                            {id:3, question:"who?"},
+                        ],
+                        "expertInfo" : {
+                        "nomineeName" : "Lorem ipsum here, nothing more",
+                        "nomineeTitle" : "Lorem ipsum here, nothing more",
+                        "nomineeFor" : "Lorem ipsum here, nothing more",
+                        "nomineeBio" : "Lorem ipsum here, nothing more"
+                      }
+                    }
+                ],
+                "amendments" : [
+                      {
+                        "_id" : "53458344e4b07375b113fab0",
+                        "name" : "Gun Control",
+                        "party" : {
+                          "_id" : "534582f7e4b07375b113faa2",
+                          "name" : "Republican",
+                          "impacts" : []
+                        },
+                        "faction" : {
+                          "_id" : "5339c22be4b01dbc9cf97bfe",
+                          "name" : "Republic Faction"
+                        },
+                        "negativeExplanation" : "Suck it",
+                        "impacts" : [
+                            {
+                              "_id" : "5339c212e4b01dbc9cf97bf6",
+                              "name" : "Impact 2 Updated",
+                              "direction" : "UP"
+                            },
+                            {
+                              "_id" : "5339c218e4b01dbc9cf97bf8",
+                              "name" : "Impact 3",
+                              "direction" : "UP"
+                            },
+                            {
+                              "_id" : "5339c21de4b01dbc9cf97bfa",
+                              "name" : "Impact 4 Updated",
+                              "direction" : "UP"
+                            }
+                        ]
+                    }
+                ]
             }
         }
     };
@@ -49,17 +129,25 @@ define(['go2o', 'jquery'], function (Go2o, $) {
          * we want to apply.
          */
         var schema = {
-            pre:  ['flattenPaths', 'renameAllIds'],
+            pre:  ['flattenPaths', 'renameAllIds', 'unwrapProvisions'],
             post: ['mergeOrphans', 'unflattenPaths'],
             preprocessors:{
                 renameAllIds:function(){
-                    console.warn('peperone')
                     Object.keys(this.flattened).forEach(function(path){
-                        console.info(path);
                         if(!path.match(/_id/)) return;
                         var out = path.replace(/_id/, 'id'),
                             value = this.flattened[path];
                         this.flattened[out] = value;
+                        delete this.flattened[path];
+                    }, this);
+                },
+                unwrapProvisions: function(){
+                    Object.keys(this.flattened).forEach(function(path){
+                        if(!path.match(/payload.bill.provisions.provisions/)) return;
+                        var out = path.replace(/payload.bill.provisions.provisions/, 'payload.bill.provisions'),
+                            value = this.flattened[path];
+                        this.flattened[out] = value;
+                        delete this.flattened[path];
                     }, this);
                 }
             },
@@ -80,8 +168,8 @@ define(['go2o', 'jquery'], function (Go2o, $) {
                 address2:{
                     collapse:'direccion'
                 },
-                'something.deep.to.see.merging':{
-                    rename:'less.levels'
+                'payload.bill.provisions.provisions':{
+                    rename: 'payload.bill.provisions'
                 }
             }
         };
